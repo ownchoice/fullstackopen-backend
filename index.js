68ae452ff1c00ce344b388e70db1a08f4fff47cc
ownchoice <ownchoice@users.noreply.github.com>
2020-09-23 10:03:23 -0300

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+
+morgan.token('mitokenpropio', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(function (tokens, req, res) {
+  if (tokens.method(req, res) === 'POST') {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.mitokenpropio(req, res)
+    ].join(' ')
+  } else {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+  }
+}))
 
 let persons = [
   {
