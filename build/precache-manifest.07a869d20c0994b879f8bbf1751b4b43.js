a0ad991b1b3f3794ceefaf30a33877a6be9d29b4
ownchoice <ownchoice@users.noreply.github.com>
2020-09-29 14:17:56 -0300

    uibuild

---
+++ build/precache-manifest.07a869d20c0994b879f8bbf1751b4b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7ea814d7d4e8373623f3bf505c6b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb1219b46c7cd3925afa",
+    "url": "/static/css/main.244092a4.chunk.css"
+  },
+  {
+    "revision": "ff4ece386108f6986f94",
+    "url": "/static/js/2.725b480e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.725b480e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1219b46c7cd3925afa",
+    "url": "/static/js/main.0cfa82fe.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
