1629f9a52a20c306e29c5bd5667c39800170b97d
ownchoice <ownchoice@users.noreply.github.com>
2020-09-29 13:29:02 -0300

    Added static build

---
+++ build/precache-manifest.9efd728f8ee224fe9777d4df293618bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6bcf1d0447dc30b8296d616497812b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3143d4f8c5628a28a25",
+    "url": "/static/css/main.244092a4.chunk.css"
+  },
+  {
+    "revision": "ff4ece386108f6986f94",
+    "url": "/static/js/2.725b480e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.725b480e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3143d4f8c5628a28a25",
+    "url": "/static/js/main.b8fcdcca.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 // const morgan = require('morgan')
 
 app.use(express.json())
