0bcce3ef6a131405bbf3fabffbbffb6044301a17
ownchoice <ownchoice@users.noreply.github.com>
2020-09-29 13:06:42 -0300

    Removed morgan

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const { response } = require('express')
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+// const morgan = require('morgan')
 
 app.use(express.json())
 // app.use(morgan('tiny'))
 
-morgan.token('mitokenpropio', function (req, res) { return JSON.stringify(req.body) })
+// morgan.token('mitokenpropio', function (req, res) { return JSON.stringify(req.body) })
 
-app.use(morgan(function (tokens, req, res) {
-  if (tokens.method(req, res) === 'POST') {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens.mitokenpropio(req, res)
-    ].join(' ')
-  } else {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-  }
-}))
+// app.use(morgan(function (tokens, req, res) {
+//   if (tokens.method(req, res) === 'POST') {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, 'content-length'), '-',
+//       tokens['response-time'](req, res), 'ms',
+//       tokens.mitokenpropio(req, res)
+//     ].join(' ')
+//   } else {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, 'content-length'), '-',
+//       tokens['response-time'](req, res), 'ms'
+//     ].join(' ')
+//   }
+// }))
 
 let persons = [
   {
