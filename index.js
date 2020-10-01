2a4e17bee67b41e00e636284699c86ea8244848f
ownchoice <ownchoice@users.noreply.github.com>
2020-10-01 13:44:23 -0300

    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -127,6 +127,22 @@ app.post('/api/persons', (request, response, next) => {
 
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person
+    .findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'Unknown endpoint'})
 }
